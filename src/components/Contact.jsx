8f62f3f544a7ffb673efb77293b8c15f42b261ac
arasu20 <imarasu20@gmail.com>
2025-02-03 19:42:44 +0530

    error cleared

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -72,7 +72,7 @@ const Contact = () => {
             />
           </label>
           <label className="flex flex-col">
-            <span className="text-white font-medium mb-4">Your Name</span>
+            <span className="text-white font-medium mb-4">Your Email</span>
             <input
             type="email"
             name="email"
