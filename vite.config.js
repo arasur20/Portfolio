89a3ff2b418d24653b3c5e93921b7890a2325460
arasu20 <imarasu20@gmail.com>
2025-02-03 18:34:31 +0530

    new file

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base:'/Portfolio/',
-  build: {
-    outDir: 'dist',
-  },
-  server: {
-    historyApiFallback: true, // Ensures React Router works correctly
-  },
 })
