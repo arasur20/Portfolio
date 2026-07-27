dc9cce8536cef70ae83474c39ef348f667c6d14e
arasu20 <imarasu20@gmail.com>
2026-07-27 11:56:37 +0530

    Bug Fix done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
-import { section } from "framer-motion/client";
 import ComputerCanvas from "./canvas/Computers";
 
 const Hero = () => {
@@ -70,7 +68,7 @@ const Hero = () => {
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center">
         <a href="#about">
           <div className="w-[35px] h-[60px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-            <motion.dev
+            <motion.div
               animate={{ y: [0, 24, 0] }}
               transition={{
                 duration: 1.5,
